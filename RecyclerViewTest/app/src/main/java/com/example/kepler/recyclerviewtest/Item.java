d734bbb9c56b58085ea12dfb76613f93ad49d7e8
Kepler <kepler710@gmail.com>
2017-05-11 17:33:29 +0900

    ha

---
+++ RecyclerViewTest/app/src/main/java/com/example/kepler/recyclerviewtest/Item.java
@@ -0,0 +1,24 @@
+package com.example.kepler.recyclerviewtest;
+
+/**
+ * Created by NAVER on 2017. 5. 11..
+ */
+
+public class Item {
+    int image;
+    String title;
+
+    public Item(int image, String title) {
+        this.image = image;
+        this.title = title;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+}
