b63370001d5716bd18ba1eda5fb24d2178636858
SoojongHwang <kepler710@gmail.com>
2017-05-15 00:38:09 +0900

    retro test

---
+++ JAVA/TEST/src/Main.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Main {
+	
+	public static void main(String[] args) {
+        String clientId = "hOBAjjmz9dUkwoGrp6pS";//애플리케이션 클라이언트 아이디값";
+        String clientSecret = "ZjcaMrNM5I";//애플리케이션 클라이언트 시크릿값";
+        try {
+            String addr = URLEncoder.encode("한가람로 446", "UTF-8");
+            String addr2 = URLEncoder.encode("127.1157168,37.5365521","UTF-8");
+            String apiURL = "https://openapi.naver.com/v1/map/geocode?query=" + addr; //json
+            String apiURL2 = "https://openapi.naver.com/v1/map/reversegeocode?query="+addr2;
+            //String apiURL = "https://openapi.naver.com/v1/map/geocode.xml?query=" + addr; // xml
+            URL url = new URL(apiURL2);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("X-Naver-Client-Id", clientId);
+            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if(responseCode==200) { // 정상 호출
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {  // 에러 발생
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+            System.out.println(response.toString());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
---
+++ ref.md
@@ -0,0 +1,4 @@
+Retrofit으로 API 호출
+
+http://stackoverflow.com/questions/42898920/add-header-parameter-in-retrofit
+http://stackoverflow.com/questions/19796235/post-with-android-retrofit
